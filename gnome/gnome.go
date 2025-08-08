f7733e09e505a7788fc301e8de5655335b4f8fb1
Tino Weick <tweick@webware-experts.de>
2025-08-08 22:45:53 +0200

    open gnome terminal in current shell

--- gnome/gnome.go
+++ gnome/gnome.go
@@ -2,19 +2,22 @@ package gnome
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 
 	sshselect "github.com/iwittkau/ssh-select"
 )
 
-// NewSSHTerminalWindow opens a new gnome-terminal window
-// gnome-terminal --profile=[PROFILE_NAME] -- [COMMAND]
+// NewSSHTerminalWindow opens an SSH connection in the current terminal window
 func NewSSHTerminalWindow(server sshselect.Server) error {
 	var cmd *exec.Cmd
 	if server.Port == "" {
-		cmd = exec.Command("gnome-terminal", fmt.Sprintf("--profile=%s", server.Profile), "--", "ssh", fmt.Sprintf("%s@%s", server.Username, server.IPAddress))
+		cmd = exec.Command("ssh", fmt.Sprintf("%s@%s", server.Username, server.IPAddress))
 	} else {
-		cmd = exec.Command("gnome-terminal", fmt.Sprintf("--profile=%s", server.Profile), "--", "ssh", "-p", server.Port, fmt.Sprintf("%s@%s", server.Username, server.IPAddress))
+		cmd = exec.Command("ssh", "-p", server.Port, fmt.Sprintf("%s@%s", server.Username, server.IPAddress))
 	}
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
 	return cmd.Run()
 }
